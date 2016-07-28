976a908407ab859c1ec951fe2e37f3caf20c9a36
cquest <cquest@openstreetmap.fr>
2016-07-28 14:53:22 +0200

    numerical values as floats instead of string

--- fr.meteo/metar/meteo-metar.py
+++ fr.meteo/metar/meteo-metar.py
@@ -13,7 +13,7 @@ cur = pg.cursor()
 txt = open(sys.argv[1])
 for station in txt:
   try:
-    # decode METAR message with python-metar 
+    # decode METAR message with python-metar
     obs = Metar.Metar(station)
     # get station data (name, location, etc)
     cur.execute("""SELECT ST_asgeojson(st_snaptogrid(wkb_geometry,0.00001)) as geom, wikidata, name FROM icao_stations WHERE icao=%s;""", (obs.station_id, ))
@@ -27,37 +27,37 @@ for station in txt:
           properties['where:wikidata']=g[1]
         if g[2] is not None:
           properties['where:name']=g[2]
-          
+
         # add available weather data
         if obs.temp is not None:
-          properties['temperature:C']=obs.temp.string('C').replace(' C','')
-          properties['temperature:F']=obs.temp.string('F').replace(' F','')
-          properties['temperature:K']=obs.temp.string('K').replace(' K','')
+          properties['temperature:C']=float(obs.temp.string('C').replace(' C',''))
+          properties['temperature:F']=float(obs.temp.string('F').replace(' F',''))
+          properties['temperature:K']=float(obs.temp.string('K').replace(' K',''))
         if obs.dewpt is not None:
-          properties['dew_point:C']=obs.dewpt.string('C').replace(' C','')
-          properties['dew_point:F']=obs.dewpt.string('F').replace(' F','')
-          properties['dew_point:K']=obs.dewpt.string('K').replace(' K','')
+          properties['dew_point:C']=float(obs.dewpt.string('C').replace(' C',''))
+          properties['dew_point:F']=float(obs.dewpt.string('F').replace(' F',''))
+          properties['dew_point:K']=float(obs.dewpt.string('K').replace(' K',''))
         if obs.press is not None:
-          properties['pressure:hPa']=obs.press.string('HPA').replace(' hPa','')
-          properties['pressure:in']=obs.press.string('IN').replace(' inches','')
+          properties['pressure:hPa']=float(obs.press.string('HPA').replace(' hPa',''))
+          properties['pressure:in']=float(obs.press.string('IN').replace(' inches',''))
         if obs.wind_dir is not None:
           properties['wind_dir:compass']=obs.wind_dir.compass()
-          properties['wind_dir:degrees']=obs.wind_dir.string().replace(' degrees','')
+          properties['wind_dir:degrees']=float(obs.wind_dir.string().replace(' degrees',''))
         if obs.wind_speed is not None:
-          properties['wind_speed:kmh']=obs.wind_speed.string('KMH').replace(' km/h','')
-          properties['wind_speed:mph']=obs.wind_speed.string('MPH').replace(' mph','')
-          properties['wind_speed:ms']=obs.wind_speed.string('MPS').replace(' mps','')
+          properties['wind_speed:kmh']=float(obs.wind_speed.string('KMH').replace(' km/h',''))
+          properties['wind_speed:mph']=float(obs.wind_speed.string('MPH').replace(' mph',''))
+          properties['wind_speed:ms']=float(obs.wind_speed.string('MPS').replace(' mps',''))
         if obs.wind_gust is not None:
-          properties['wind_gust:kmh']=obs.wind_gust.string('KMH')
-          properties['wind_gust:mph']=obs.wind_gust.string('MPH').replace(' mph','')
-          properties['wind_gust:ms']=obs.wind_gust.string('MPS').replace(' mps','')
+          properties['wind_gust:kmh']=float(obs.wind_gust.string('KMH'))
+          properties['wind_gust:mph']=float(obs.wind_gust.string('MPH').replace(' mph',''))
+          properties['wind_gust:ms']=float(obs.wind_gust.string('MPS').replace(' mps',''))
         if obs.vis is not None:
           properties['visibility:km']=obs.vis.string('KM').replace('greater than ','>').replace(' km','')
           properties['visibility:miles']=obs.vis.string('MI').replace('greater than ','>').replace(' miles','')
 
         # create geojson
         geojson = json.dumps(dict(type='Feature', properties=properties, geometry=json.loads(g[0])),sort_keys=True)
-        
+
         # send it to OEDB API
         r = requests.post(api+'/event', data = geojson)
         if r.status_code == 201 :
