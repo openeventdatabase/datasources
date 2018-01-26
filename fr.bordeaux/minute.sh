bb4cd86c965f77d9579b49df0069c792b54a002a
cquest <cquest@openstreetmap.fr>
2018-01-26 18:58:44 +0100

    màj fr.bordeaux + ajout archivage

--- fr.bordeaux/minute.sh
+++ fr.bordeaux/minute.sh
@@ -1,7 +1,12 @@
+HIERYY=$(date --date="-1 day" +'%Y')
+HIERMM=$(date --date="-1 day" +'%m')
+HIER=$(date --date="-1 day" +'%Y-%m-%d')
+
 mkdir -p `date +%Y-%m`
 f=`date +%Y-%m`/trafic-`date +%Y-%m-%dT%H:%M:00%Z`.csv
 l="last-trafic.csv"
-curl "http://data.bordeaux-metropole.fr/files.php?gid=80&format=6" -s > $f
+curl -L "https://data.bordeaux-metropole.fr/files.php?gid=80&format=6" -s > $f
+
 # ~/.virtualenvs/oedb/bin/python trafic.py
 if diff $f $l  >/dev/null
 then
@@ -14,7 +19,7 @@ fi
 mkdir -p `date +%Y-%m`
 f=`date +%Y-%m`/message-`date +%Y-%m-%dT%H:%M:00%Z`.csv
 l="last-messages.csv"
-curl "http://data.bordeaux-metropole.fr/files.php?gid=599&format=6" -s | iconv -f iso8859-1 -t utf-8 - > $f
+curl -L "https://data.bordeaux-metropole.fr/files.php?gid=599&format=6" -s | iconv -f iso8859-1 -t utf-8 - > $f
 # ~/.virtualenvs/oedb/bin/python trafic.py
 if diff $f $l  >/dev/null
 then
@@ -23,3 +28,5 @@ then
 else
   ln -fs $f $l
 fi
+
+7z a $HIERYY.7z $HIERYY-$HIERMM/*$HIER* && rm $HIERYY-$HIERMM/*$HIER*
