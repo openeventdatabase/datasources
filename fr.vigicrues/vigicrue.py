4f1410e69cbee6c1c4f8864240379678ad6162cd
cquest <cquest@openstreetmap.fr>
2018-01-29 09:29:44 +0100

    mise au propre

--- fr.vigicrues/vigicrue.py
+++ fr.vigicrues/vigicrue.py
@@ -19,13 +19,13 @@ with open('vigicrue-stations.geojson') as json_file:
         if 'Serie' in mesures:
             nom_station = mesures['Serie']['LbStationHydro']
             mm = mesures['Serie']['ObssHydro']
+            #print(s['properties']['id'], nom_station)
             for i in range(1,len(mm)):
                 m = mm[len(mm)-i]
                 when = m['DtObsHydro']
-                print(when, m['ResObsHydro'])
                 geojson = json.dumps(dict(type='Feature',properties=dict(source='http://www.vigicrues.gouv.fr/', type='observed', what='water.level', when=when, hauteur=m['ResObsHydro'], id_station=s['properties']['id'], label=nom_station+" : "+str(m['ResObsHydro'])+"m"), geometry=s['geometry']))
                 # envoi à l'API OpenEventDatabase
-                print(geojson)
+                #print(geojson)
                 r = requests.post('http://localhost:8000/event', data = geojson)
                 if r.status_code != 201:
                     break
